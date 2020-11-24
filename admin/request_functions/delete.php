70dc9d1d0c018b95fc9f5804c1f3af81a418c59f
munaALboashi <munaelboashi@gmail.com>
2020-11-24 12:06:37 +0200

    laravel change

--- admin/request_functions/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../includes/db_connection.php';
-$id = $_POST['ID'];
-echo $id;
-$query1 = $con->prepare("DELETE FROM projects WHERE ID = ?");
-
-$query1->execute(array($id));
---
+++ database/migrations/2020_11_24_095530_create_jobs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('applicant_name');
+            $table->string('applicant_email');
+            $table->string('applicant_cv');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
